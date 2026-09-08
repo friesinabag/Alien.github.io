1ad80cb1e7508ddf3f831d885c1ff81d4ea2fc18
tungtangle <sethohara33@gmail.com>
2026-09-08 17:44:15 +0100

    Update game.js

--- game.js
+++ game.js
@@ -8728,6 +8728,12 @@ function initGameUI() {
     $("applyCustomRolesButton").onclick =
         applyCustomRoles;
 
+    $("createRoomButton").onclick =
+    createOnlineRoom;
+
+    $("joinRoomButton").onclick =
+    joinOnlineRoom;
+
     /*
        Make sure mobile RANDOM is rebound after
        the rest of the setup UI has loaded.
